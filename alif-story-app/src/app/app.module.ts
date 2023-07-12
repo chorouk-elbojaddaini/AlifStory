a6edd8c14c7d3345ebdd7aeb1cea15ad62892cac
chorouk-elbojaddaini <choroukelbojaddaini001@gmail.com>
2023-07-12 01:06:52 +0100

    home-page

--- alif-story-app/src/app/app.module.ts
+++ alif-story-app/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './sharepage/navbar/navbar.component';
 import { FooterComponent } from './sharepage/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,12 @@ import { FooterComponent } from './sharepage/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- alif-story-app/src/app/home/home.component.ts
+++ alif-story-app/src/app/home/home.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+   cards =[
+    {title:'DES RÉCITS AUTHENTIQUES',
+  description:'offre aux enfants la possibilité'},
+  {title:'UN ACCOMPAGNEMENT ÉDUCATIF COMPLET',
+  description:'En plus des récits, ils inclusent'},
+  {title:'UN ACCOMPAGNEMENT ÉDUCATIF COMPLET',
+  description:'En plus des récits, ils inclusent'},
+  {title:'UN ACCOMPAGNEMENT ÉDUCATIF COMPLET',
+  description:'En plus des récits, ils inclusent'}
+   ]
 }
